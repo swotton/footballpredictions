a7758958429c1f14b51bc1704dbf63dfb516d5d8
Steve Wotton <steven.wotton@tesco.com>
2019-10-20 08:23:32 +0100

    Initial commit

---
+++ src/main/java/com/stevewotton/Fixture.java
@@ -0,0 +1,44 @@
+package com.stevewotton;
+
+import java.util.Date;
+
+public class Fixture {
+
+    private Date fixtureDate;
+    private String fixtureDivision;
+    private String fixtureHomeTeam;
+    private String fixtureAwayTeam;
+
+    public Date getFixtureDate() {
+        return fixtureDate;
+    }
+
+    public void setFixtureDate(Date fixtureDate) {
+        this.fixtureDate = fixtureDate;
+    }
+
+    public String getFixtureDivision() {
+        return fixtureDivision;
+    }
+
+    public void setFixtureDivision(String fixtureDivision) {
+        this.fixtureDivision = fixtureDivision;
+    }
+
+    public String getFixtureHomeTeam() {
+        return fixtureHomeTeam;
+    }
+
+    public void setFixtureHomeTeam(String fixtureHomeTeam) {
+        this.fixtureHomeTeam = fixtureHomeTeam;
+    }
+
+    public String getFixtureAwayTeam() {
+        return fixtureAwayTeam;
+    }
+
+    public void setFixtureAwayTeam(String fixtureAwayTeam) {
+        this.fixtureAwayTeam = fixtureAwayTeam;
+    }
+
+}
