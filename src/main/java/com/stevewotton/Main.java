a83b558993763e04d5783a5300b2c22f27110da1
Steve Wotton <steven.wotton@tesco.com>
2019-10-20 08:34:53 +0100

    Created main class

---
+++ src/main/java/com/stevewotton/Main.java
@@ -0,0 +1,13 @@
+package com.stevewotton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO
+        //  Read fixtures file
+        //  Create fixture objects
+        //  Create stats objects
+        //  Create predictions
+        //  Write predictions to file
+    }
+}
